0bac419b6d28f1ae7e8eb094fc9e1da7ae9462ae
Lawson-JR <140719079+Lawson-JR@users.noreply.github.com>
2024-06-24 17:41:16 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;  // Block-scoped variable
-    let task2 = false;  // Block-scoped variable
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
